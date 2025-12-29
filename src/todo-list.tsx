174f8457a1d55a8e75f7a62bc68d0eef31a2229a
Teresa Lew <teresalew92@gmail.com>
2025-12-29 14:48:49 -0800

    feat: add inline edit mode

--- src/todo-list.tsx
+++ src/todo-list.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { Button } from "./components/ui/button";
 import type { Todo } from "./types";
 
 interface TodoListProps {
   todos: Todo[];
   handleToggleTodo: (id: string) => void;
+  handleUpdateTodo: (id: string, updatedTodo: Todo) => void;
 }
 
 export default function TodoList({ todos, handleToggleTodo }: TodoListProps) {
+  const [isInEditMode, setIsInEditMode] = useState<string[]>([]);
+
+  function handleEditMode(id: string) {
+    setIsInEditMode((prev) => [...prev, id]);
+  }
+
+  function isEditing(id: string) {
+    return isInEditMode.find((todoId) => todoId === id);
+  }
+
   return (
     <ol className="flex w-full flex-col space-y-4">
       {todos.map(({ id, title, completed }: Todo) => {
@@ -17,19 +29,31 @@ export default function TodoList({ todos, handleToggleTodo }: TodoListProps) {
             key={id}
             className="flex items-center justify-between rounded-sm p-4 outline-1"
           >
-            <div>
-              <label htmlFor={todoId}>
-                <input
-                  type="checkbox"
-                  checked={completed}
-                  id={todoId}
-                  onChange={() => handleToggleTodo(id)}
-                  className="mr-2"
-                />
-                {title}
-              </label>
-            </div>
-            <Button onClick={() => console.log("edit item")}>Edit</Button>
+            {isEditing(id) ? (
+              <>
+                <input value={title} />
+                <Button variant="ghost" onClick={() => console.log("saving!")}>
+                  Save
+                </Button>
+              </>
+            ) : (
+              <>
+                <label htmlFor={todoId}>
+                  <input
+                    type="checkbox"
+                    checked={completed}
+                    id={todoId}
+                    onChange={() => handleToggleTodo(id)}
+                    className="mr-2"
+                  />
+                  {title}
+                </label>
+
+                <Button variant="ghost" onClick={() => handleEditMode(id)}>
+                  Edit
+                </Button>
+              </>
+            )}
           </li>
         );
       })}

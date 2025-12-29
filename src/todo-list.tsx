b8735148cea9b98adb574702283240fc93403035
Teresa Lew <teresalew92@gmail.com>
2025-12-29 14:25:05 -0800

    feat: add edit button with placeholder handler

--- src/todo-list.tsx
+++ src/todo-list.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button";
 import type { Todo } from "./types";
 
 interface TodoListProps {
@@ -7,19 +8,28 @@ interface TodoListProps {
 
 export default function TodoList({ todos, handleToggleTodo }: TodoListProps) {
   return (
-    <ol className="flex w-full flex-col gap-4">
+    <ol className="flex w-full flex-col space-y-4">
       {todos.map(({ id, title, completed }: Todo) => {
         const todoId = `todo-${id}`;
 
         return (
-          <li key={id}>
-            <input
-              type="checkbox"
-              checked={completed}
-              id={todoId}
-              onChange={() => handleToggleTodo(id)}
-            />
-            <label htmlFor={todoId}>{title}</label>
+          <li
+            key={id}
+            className="flex items-center justify-between rounded-sm p-4 outline-1"
+          >
+            <div>
+              <label htmlFor={todoId}>
+                <input
+                  type="checkbox"
+                  checked={completed}
+                  id={todoId}
+                  onChange={() => handleToggleTodo(id)}
+                  className="mr-2"
+                />
+                {title}
+              </label>
+            </div>
+            <Button onClick={() => console.log("edit item")}>Edit</Button>
           </li>
         );
       })}
